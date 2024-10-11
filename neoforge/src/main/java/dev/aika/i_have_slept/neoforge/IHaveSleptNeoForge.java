f9559ec63f7b064f5b74e0c77d49363e9edb48f0
Gizmo <me@aika.dev>
2024-10-12 05:59:30 +0800

    refactor: 1.21 port

---
+++ neoforge/src/main/java/dev/aika/i_have_slept/neoforge/IHaveSleptNeoForge.java
@@ -0,0 +1,12 @@
+package dev.aika.i_have_slept.neoforge;
+
+import dev.aika.i_have_slept.IHaveSlept;
+import net.neoforged.fml.common.Mod;
+
+@Mod(IHaveSlept.MOD_ID)
+public final class IHaveSleptNeoForge {
+    public IHaveSleptNeoForge() {
+        // Run our common setup.
+        IHaveSlept.init();
+    }
+}
