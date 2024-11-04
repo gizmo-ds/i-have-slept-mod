9d642dd8605a750aa2c58790b61aa4177ea41ee4
Gizmo <me@aika.dev>
2024-11-05 01:41:21 +0800

    fix: resolve crash caused by GameRules.register in Fabric #1

--- forge/src/main/java/dev/aika/i_have_slept/forge/IHaveSleptForge.java
+++ forge/src/main/java/dev/aika/i_have_slept/forge/IHaveSleptForge.java
@@ -1,12 +1,13 @@
 package dev.aika.i_have_slept.forge;
 
 import dev.aika.i_have_slept.IHaveSlept;
+import net.minecraft.world.GameRules;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod(IHaveSlept.MOD_ID)
 public final class IHaveSleptForge {
     public IHaveSleptForge() {
-        // Run our common setup.
+        IHaveSlept.BETTER_SERVER_SLEEP = GameRules.register("doBetterServerSleep", GameRules.Category.SPAWNING, GameRules.BooleanRule.create(true));
         IHaveSlept.init();
     }
 }
