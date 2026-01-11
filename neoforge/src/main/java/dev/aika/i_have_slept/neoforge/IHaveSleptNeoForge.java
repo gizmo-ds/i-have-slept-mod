c2829fca584382d1041e0c5f96af03cb14e7aab2
Gizmo <me@aika.dev>
2026-01-11 21:36:36 +0800

    refactor: change mappings

--- neoforge/src/main/java/dev/aika/i_have_slept/neoforge/IHaveSleptNeoForge.java
+++ neoforge/src/main/java/dev/aika/i_have_slept/neoforge/IHaveSleptNeoForge.java
@@ -1,13 +1,11 @@
 package dev.aika.i_have_slept.neoforge;
 
 import dev.aika.i_have_slept.IHaveSlept;
-import net.minecraft.world.GameRules;
 import net.neoforged.fml.common.Mod;
 
 @Mod(IHaveSlept.MOD_ID)
 public final class IHaveSleptNeoForge {
     public IHaveSleptNeoForge() {
-        IHaveSlept.BETTER_SERVER_SLEEP = GameRules.register("doBetterServerSleep", GameRules.Category.SPAWNING, GameRules.BooleanRule.create(true));
         IHaveSlept.init();
     }
 }
